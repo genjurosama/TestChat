45c3a177351f5139ba7b50aee4d956cf2a24c24f
ciwolsey <carl@wolsey.org>
2015-05-19 13:32:09 +0000

    add untracked email template/events

---
+++ client/pages/email/templates/froala_email.js
@@ -0,0 +1,104 @@
+Template.froalaEmail.onRendered(function(t){
+	
+	this.$('#froala-email')
+	this.$('#froala-email').editable({
+	inlineMode: false,
+	buttons: ['clear', 'customerVariables', 'recipientVariables'],
+	height: '400',
+	customDropdowns: {
+    customerVariables: {
+      title: 'Customer Variables',
+      label: 'hello',
+      icon: {
+        type: 'font',
+        value: 'fa fa-plus-square',
+      },
+      options: {
+        'Signature': function () {
+          this.insertHTML('[c.signature]');
+        },
+        'First Name': function () {
+          this.insertHTML('[c.fname]');
+        },
+        'Last Name': function () {
+          this.insertHTML('[c.surname]');
+        },
+        'SSN': function () {
+          this.insertHTML('[c.ssn]');
+        },
+        'DOB': function () {
+          this.insertHTML('[c.dob]');
+        },
+        'Address Line 1': function () {
+          this.insertHTML('[c.addr1]');
+        },
+        'Address Line 2': function () {
+          this.insertHTML('[c.addr2]');
+        },
+        'City': function () {
+          this.insertHTML('[c.city]');
+        },
+        'State': function () {
+          this.insertHTML('[c.state]');
+        },
+        'Zip': function () {
+          this.insertHTML('[c.zip]');
+        },
+        'Phone 1': function () {
+          this.insertHTML('[c.phone1]');
+        },
+        'Phone 2': function () {
+          this.insertHTML('[c.phone2]');
+        },
+        'Fax': function () {
+          this.insertHTML('[c.fax]');
+        }
+      },
+      refresh: function () {
+        // This method is called when the state of the button might have been changed.
+      },
+      refreshOnShow: function () {
+        // This method is called when the dropdown context menu is shown.
+      }
+    },
+    recipientVariables: {
+      title: 'Custorg',
+      icon: {
+        type: 'font',
+        value: 'fa fa-plus-square',
+      },
+      options: {
+        'Name': function () {
+          this.insertHTML('[r.name');
+        },
+        'Address 1': function () {
+          this.insertHTML('[r.addr1');
+        },
+        'Address 2': function () {
+          this.insertHTML('[r.addr2');
+        },
+        'City': function () {
+          this.insertHTML('[r.city]');
+        },
+        'State': function () {
+          this.insertHTML('[r.state]');
+        },
+        'Zip': function () {
+          this.insertHTML('[r.zip]');
+        },
+        'Phone 1': function () {
+          this.insertHTML('[r.phone1]');
+        },
+        'Phone 2': function () {
+          this.insertHTML('[r.phone2]');
+        },
+        'Fax': function () {
+          this.insertHTML('[r.fax]');
+        }
+      }
+    }
+  }
+});
+
+});
+
