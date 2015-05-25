9d54cbb9c4d36603691a8a8420725e17115cbd28
ciwolsey <carl@wolsey.org>
2015-05-25 13:57:58 +0000

    Don't output froalaOptions as html attribs

--- client/pages/email/templates/froala_email/froala_email.js
+++ client/pages/email/templates/froala_email/froala_email.js
@@ -1,7 +1,17 @@
 Template.afFroalaEmail.onRendered(function(){
   console.log(this);
   var id = this.data.atts.id;
-  $('#'+id).editable({inlineMode: false});
+  $('#'+id).editable(this.data.atts.froalaOptions);
+});
+
+Template.afFroalaEmail.helpers({
+  atts: function(){
+    var atts = _.clone(this.atts);
+
+    // Remove froalaOptions so doesn't get rendered as html attrib
+    delete atts.froalaOptions;
+    return atts;
+  }
 });
 
 AutoForm.addInputType('froalaEmail', {
@@ -9,4 +19,5 @@ AutoForm.addInputType('froalaEmail', {
 		valueOut: function(){
 			return $(this).editable('getHTML', true, true);
 		}
+    //TODO: Implement valueIn
 });
